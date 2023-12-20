1a60bbed31afc2dc229db1e89f61496c63932435
Jishap-Tomsher <130639775+Jishap-Tomsher@users.noreply.github.com>
2023-12-20 12:20:08 +0400

    admin robots

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta name="robots" content="noindex">
     <title>{{ env('APP_NAME') }} - {{ $title ?? '' }}</title>
     <link rel="icon" type="image/svg" href="{{ asset('adminassets/img/favicon.png') }}">
     <link rel="stylesheet" href="{{ adminAsset('font/iconsmind-s/css/iconsminds.css') }}" />
